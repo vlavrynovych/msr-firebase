c3f80fc87c071bec7b79cad56a22bc893b15d364
Volodymyr Lavrynovych <volodyalavrynovych@gmail.com>
2023-12-03 02:45:02 +0200

    #2: Attempt to simplify config by moving all the steps needed CI to the bash file

---
+++ scripts/ci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script should be executed only on CI (like CircleCI or TravisCI)
+# We expect that PRIVATE_KEY and DATABASE_URL are already provided by CI config
+
+# creates directories
+mkdir $PWD/backups  # is needed for tests
+mkdir $PWD/key      # is needed for private key
+
+# prepares private key
+export GOOGLE_APPLICATION_CREDENTIALS="$PWD/key/private_key.json"
+echo $GOOGLE_APPLICATION_CREDENTIALS
+echo $PRIVATE_KEY > $GOOGLE_APPLICATION_CREDENTIALS
