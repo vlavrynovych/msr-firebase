c262d94fc7a8069a54ce438c3508b0d7d2e10889
Volodymyr Lavrynovych <volodyalavrynovych@gmail.com>
2023-12-11 12:23:25 +0200

    #11: No need to duplicate all the parameters

--- src/model/MigrationInfo.ts
+++ src/model/MigrationInfo.ts
@@ -3,10 +3,4 @@ import {MigrationScript} from "migration-script-runner";
 
 export class MigrationInfo extends MigrationScript implements IEntity{
     key: string | undefined;
-    finishedAt!: number;
-    name!: string;
-    result!: string;
-    startedAt!: number;
-    timestamp!: number;
-    username!: string;
 }
